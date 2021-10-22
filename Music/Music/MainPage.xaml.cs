9e019302ed53e2203bfed50fd1ccf182d56d04af
bashobbel <bashobbel@gmail.com>
2021-10-22 16:47:26 +0200

    Add project files.

---
+++ Music/Music/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Music
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            var player = Plugin.SimpleAudioPlayer.CrossSimpleAudioPlayer.Current; player.Load("Drup.mp3"); player.Play();
+        }
+    }
+}
